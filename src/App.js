e0bae8f4166891f33b879488dde82aa8629c4087
Afonso Albrecht <a.schulzalbrecht@gmail.com>
2021-06-16 17:10:21 +0200

    create feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React -----> this is on a feature branch
         </a>
       </header>
     </div>
